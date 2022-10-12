735f01bd0b92fa856894a0f96bedbf5210d37c90
Михайлова Валерия Романовна <114227035+valerrron23@users.noreply.github.com>
2022-10-12 11:31:25 +0300

    Add files via upload

---
+++ 5zad07-10.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Hello
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("a = "); //программа просит пользователя ввести значение переменной вещественного типа
+            double a = Convert.ToDouble(Console.ReadLine()); //вывод данных
+            Console.Write("b = "); //программа просит пользователя ввести значение переменной вещественного типа
+            double b = Convert.ToDouble(Console.ReadLine()); //вывод данных
+            Console.WriteLine("{1:F2}/{0:F2}={2:F2}", a, b, a/b); // программа вычисляет введёные данные по формуле
+            Console.WriteLine("Для продолжения нажмите нажмите любую клавишу..."); //Программа ждёт пока пользователь нажмёт  любую клавишу 
+        }
+    }
+}
